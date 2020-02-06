1bd7d5b4fb6284a070aa315784f6967756085456
TAB_mk <tabmk999@gmail.com>
2020-02-07 02:55:23 +0300

    new code, changes in readme

--- huifikator.js
+++ huifikator.js
@@ -1,23 +1,70 @@
-String.prototype.huifikacia = function (prefix = 'ху') {
-	let doHui = word => {
-        /** @type {String} перевод текущей строки в нижний регистр */
-        str = word.toLowerCase();
-        /** @type {Object} Хардкод гласных. И мне и железу легче просто брать их из объекта */
-        let g = { 'а' : 'я', 'о' : 'ё', 'э' : 'е', 'ы' : 'и', 'у' : 'ю', 'я' : 'а', 'ё' : 'о', 'е' : 'е', 'и' : 'ы', 'ю' : 'у' };
-        /** @type {?Array} регулярка на гласные */
-        let firstGl = str.match(/([аоэыуяёеию])/gi);
-        if (firstGl && firstGl.length) {
-            // Первые 2 гласные могут быть одинаковые, поэтому просто удаляем первую, чтобы все работало нормально
-            if (firstGl.length >= 3 && firstGl[0] === firstGl[1]) str = str.replace(firstGl[0], '');
-            /** @type {Number} Делим слово на слоги. Если 3+, то начинаем хуефикацию со второго */
-            let s = firstGl.length >= 3 ? 1 : 0;
-            /** @type {String} Первая гласная в нижнем регистре */
-            let para = firstGl[s].toLowerCase()
-            // строка 'префикс + новая дифтонговая гласная, либо наоборот + продолжение строки'
-            return `${prefix}${g[para]}${str.slice(str.indexOf(para)+1)}`;
-        }
-	}
+/**
+ * conver string
+ * @param  {String} text
+ * @param  {String} [prefix='ху']
+ * @return {String}
+ */
+const huifikacia = (text, prefix = 'ху') => {
+  /**
+  * Check letter case
+  * @param  {String}  letter
+  * @return {Boolean}
+  */
+  const checkCase = (letter) => !!(letter === letter.toUpperCase());
+  /**
+   * convert single word
+   * @param  {String} singleWord
+   * @return {String}
+   */
+  const doHui = (singleWord) => {
+    /** @type {String} word to convert **/
+    let word = singleWord;
+    /** @type {String} word prefix **/
+    let pref = prefix;
+    /** @type {String} vowel table with replacers **/
+    const vowelList = {
+      а: 'я',
+      о: 'ё',
+      э: 'е',
+      ы: 'и',
+      у: 'ю',
+      я: 'а',
+      ё: 'о',
+      е: 'е',
+      и: 'ы',
+      ю: 'у',
+    };
 
-	return this.split(' ').map(doHui).join(' ');
-	
+    /** @type {?Array} find vowels **/
+    const vowels = word.match(/([аоэыуяёеию])/gi);
+
+    if (vowels && vowels.length) {
+      // first two vowels can't be same. Deleting first.
+      if (vowels.length >= 3 && vowels[0].toLowerCase() === vowels[1].toLowerCase()) {
+        word = word.replace(vowels[0], '');
+      }
+      /**
+       * check if word have >= 3 syllables and conver Boolean to Number
+       * @type {Number}
+       */
+      const syllable = Number(!!(vowels.length >= 3));
+      /** @type {String} replaced vowel **/
+      const diphthong = vowelList[vowels[syllable]];
+      // make first char of converted work same case with origin
+      if (checkCase(word.charAt(0))) {
+        pref = pref.charAt(0).toUpperCase() + pref.slice(1);
+      }
+      return `${pref}${diphthong}${word.slice(word.indexOf(vowels[syllable]) + 1)}`;
+    }
+    // if no vowel detectet -- return same word
+    return text;
+  };
+
+  // split string to words, convert each and make single string back
+  return text.split(' ').map(doHui).join(' ');
+};
+
+// bind function to String
+String.prototype.huifikacia = function(prefix) {
+  return huifikacia(this, prefix)
 };
